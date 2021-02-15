589f09f5ec0cf6e3725019002ccfc1945f82ea9b
samantha <nsamanthaduclaire@gmail.com>
2021-02-15 13:04:17 +0100

    first commit

--- app/src/main/java/com/example/my_android_projet/MainActivity.java
+++ app/src/main/java/com/example/my_android_projet/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.my_android_projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
